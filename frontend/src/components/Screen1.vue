67c07fba1cf211ae97ef96dd36ddb9b1ae945cf2
roipicsou <pl163330@gmail.com>
2024-11-13 14:59:02 +0100

    time v1

--- frontend/src/components/Screen1.vue
+++ frontend/src/components/Screen1.vue
@@ -1,12 +1,52 @@
 <template>
-    <div>
-      <h2>Écran 1 lol</h2>
-      <p>Contenu spécifique à l'écran 1.</p>
-    </div>
-  </template>
-  
+  <div class="screen-container">
+    <p>Date : {{ currentDate }}</p>
+    <p>Heure : {{ currentTime }}</p>
+  </div>
+</template>
+
 <script>
-  export default {
-    name: 'Screen1'
-  }
-</script>  
+export default {
+  data() {
+    return {
+      currentDate: '',
+      currentTime: '',
+      timer: null,
+    };
+  },
+  methods: {
+    updateDateTime() {
+      const now = new Date();
+      this.currentDate = now.toLocaleDateString();
+      this.currentTime = now.toLocaleTimeString();
+    },
+  },
+  created() {
+    this.updateDateTime();
+    this.timer = setInterval(this.updateDateTime, 1000);
+  },
+  beforeDestroy() {
+    clearInterval(this.timer);
+  },
+};
+</script>
+
+<style scoped>
+.screen-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* S'assure que le conteneur prend toute la hauteur de l'écran */
+  text-align: center; /* Centre le texte */
+}
+
+h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+p {
+  font-size: 18px;
+}
+</style>
